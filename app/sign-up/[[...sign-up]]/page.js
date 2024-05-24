cea1a3ed54ef1dc0275f1a1218e81b78f35630bb
Rian Perassoli <rian.perassoli@gmail.com>
2024-05-23 22:18:54 -0300

    finish creating the sign in and sign up customized pages from clerk

---
+++ app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  <div className="min-h-screen flex justify-center items-center">
+    <SignUp />
+  </div>;
+};
+
+export default SignUpPage;
