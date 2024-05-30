7af919b54ef3eaab6911a6350317e8d39eeaafd2
Rian Perassoli <rian.perassoli@gmail.com>
2024-05-30 12:21:01 -0300

    creating sidebar and its components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,17 @@
+import MemberProfile from "./MemberProfile";
+import NavLinks from "./NavLinks";
+import SidebarHeader from "./SidebarHeader";
+
 const Sidebar = () => {
-  return <div className="text-6xl">Sidebar</div>;
+  return (
+    <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid gird-rows-[auto,1fr,auto]">
+      <SidebarHeader />
+
+      <NavLinks />
+
+      <MemberProfile />
+    </div>
+  );
 };
 
 export default Sidebar;
