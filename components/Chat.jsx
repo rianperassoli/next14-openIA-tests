cf47c8039d3dc47201de06101b3f6ebf3051fee4
Rian Perassoli <rian.perassoli@gmail.com>
2024-06-20 16:19:33 -0300

    creating chat and displaying messages on chat screen

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -3,27 +3,55 @@
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { generateChatResponse } from "../utils/action";
+import toast from "react-hot-toast";
 
 const Chat = () => {
   const [text, setText] = useState("");
-  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-  const { mutate: createMessage } = useMutation({
-    mutationFn: (message) => {
-      generateChatResponse(message);
+  const { mutate, isPending } = useMutation({
+    mutationFn: (query) => generateChatResponse([...messages, query]),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error("Something went wrong...");
+        return;
+      }
+      setMessages((prev) => [...prev, data]);
     },
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    createMessage(text);
+    const query = { role: "user", content: text };
+    mutate(query);
+
+    setMessages((prev) => [...prev, query]);
+    setText("");
   };
 
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div className="">
-        <h2 className="text-5xl">messages</h2>
+        {messages.map(({ role, content }, index) => {
+          const avatar = role === "user" ? "👤" : "🤖";
+          const bgColor = role === "user" ? "bg-base-200" : "bg-base-100";
+          return (
+            <div
+              key={index}
+              className={` ${bgColor} flex py-6 -mx-8 px-8
+               text-xl leading-loose border-b border-base-300`}
+            >
+              <span className=" mr-4">{avatar}</span>
+              <p className="max-w-3xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending && (
+          <div className="flex items-center justify-center w-full h-20">
+            <span className="loading loading-dots loading-lg bg-primary opacity-30" />
+          </div>
+        )}
       </div>
 
       <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
@@ -36,8 +64,13 @@ const Chat = () => {
             onChange={(e) => setText(e.target.value)}
             required
           />
-          <button className="btn btn-primary join-item" type="submit">
-            ask question
+          <button
+            className="btn btn-primary join-item"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending && <span className="loading loading-bars loading-xs" />}
+            {isPending ? "please wait..." : "ask question"}
           </button>
         </div>
       </form>
